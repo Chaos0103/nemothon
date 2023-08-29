1801eb80081cea7897e2c83cf4eba3a7629d3587
Chaos0103 <lyt1228@naver.com>
2023-08-29 20:37:42 +0900

    feat: BottomNav 생성

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,19 +2,23 @@ import './App.css';
 import {Route, Routes} from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import Footer from "./components/Footer";
-import './components/footer.css';
+import BottomNav from "./components/BottomNav";
+import './components/bottomNav.css';
+import DailyPlan from "./components/DailyPlan";
+import MyPage from "./pages/MyPage";
 
 const App = () => {
     return (
         <div className="App">
-            <div className=''>
+            <div className='wrapper'>
                 <Routes>
                     <Route path="/" element={<Home/>}/>
                     <Route path="/login" element={<Login/>}/>
+                    <Route path="/daily" element={<DailyPlan/>}/>
+                    <Route path="/my" element={<MyPage/>}/>
                 </Routes>
             </div>
-            <Footer/>
+            <BottomNav/>
         </div>
     );
 };
