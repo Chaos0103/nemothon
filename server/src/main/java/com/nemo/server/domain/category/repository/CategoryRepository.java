ee72237a6d9e4a378d3aaa55dcda9d5a31b3447b
leeyr0412 <leeyr0412@naver.com>
2023-08-31 10:36:44 +0900

    feat: CategoryRepository 생성

---
+++ server/src/main/java/com/nemo/server/domain/category/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.nemo.server.domain.category.repository;
+
+import com.nemo.server.domain.category.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
