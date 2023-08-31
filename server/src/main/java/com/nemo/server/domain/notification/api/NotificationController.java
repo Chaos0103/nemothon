e488d5aa0b2f1b4ee76e6c3bd06a813bcefea2b8
tngus2sh <ssuhyeon98@gmail.com>
2023-08-31 11:05:53 +0900

    refactor: move files

--- server/src/main/java/com/nemo/server/domain/notification/api/NotificationController.java
+++ server/src/main/java/com/nemo/server/domain/notification/api/NotificationController.java
@@ -1,6 +1,6 @@
-package com.nemo.server.notification.api;
+package com.nemo.server.domain.notification.api;
 
-import com.nemo.server.notification.service.NotificationService;
+import com.nemo.server.domain.notification.service.NotificationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
