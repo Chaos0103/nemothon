69854972073f1b26b333c5c4ef0f925a99dd665b
Chaos0103 <lyt1228@naver.com>
2023-08-31 12:38:15 +0900

    feat: 랜덤 색상 코드 구현

--- server/src/main/java/com/nemo/server/api/service/category/CategoryService.java
+++ server/src/main/java/com/nemo/server/api/service/category/CategoryService.java
@@ -9,9 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
@@ -23,6 +21,7 @@ public class CategoryService {
     private final CategoryRepository repository;
 
     public CategoryResponse add(AddCategoryDto dto) {
+        dto.setColorCode(randomColorCode());
         Category category = dto.toEntity();
         Category savedCategory = repository.save(category);
         return CategoryResponse.of(savedCategory);
@@ -51,4 +50,14 @@ public class CategoryService {
     public void remove(Long categoryId) {
         repository.deleteById(categoryId);
     }
+
+    private static final String[] COLOR_CODE = {"F4989A", "94D994", "58CDE5", "F8D33C"};
+
+    private String randomColorCode() {
+        Random random = new Random();
+        int index = random.nextInt(4);
+        return COLOR_CODE[index];
+    }
+
+    //
 }
