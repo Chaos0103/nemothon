4323729f722ab83eea347316a940314b552d34b3
tngus2sh <ssuhyeon98@gmail.com>
2023-09-01 05:42:38 +0900

    refactor: noti moidfy

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,23 +1,24 @@
 import { Link } from "react-router-dom";
-import {useEffect} from "react";
+import { useEffect } from "react";
 import MonthPlan from "../components/MonthPlan";
 import Notification from "../components/Notification";
+import TopNav from "../components/TopNav";
 
 const Home = () => {
-    useEffect(() => {
-        // 로컬스토리지에서 뽑아와서, 토큰으로 사용자 정보를 요청하는 api 함수
-        // 리코일, 리덕스
-    }, []);
+  useEffect(() => {
+    // 로컬스토리지에서 뽑아와서, 토큰으로 사용자 정보를 요청하는 api 함수
+    // 리코일, 리덕스
+  }, []);
 
-
-    return (
-        <div>
-            <div className='top-bar'></div>
-            {/*<Notification></Notification>*/}
-            <Link to="/search">search</Link>
-            <MonthPlan/>
-        </div>
-    );
+  return (
+    <div>
+      <TopNav />
+      <div className="top-bar"></div>
+      <Notification></Notification>
+      <Link to="/search">search</Link>
+      <MonthPlan />
+    </div>
+  );
 };
 
 export default Home;
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,80 +1,78 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import axios from "axios";
-import {TextField, Button, Container, Box, Grid} from "@mui/material";
-import {useNavigate} from "react-router-dom";
+import { TextField, Button, Container, Box, Grid } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [loggedIn, setLoggedIn] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loggedIn, setLoggedIn] = useState(false);
 
-    const login = () => {
-
-        const json = {
-            'email': email,
-            'password': password
-        }
-
-        console.log(json);
-
-        axios
-            .post('/api/login', json)
-            .then((response) => {
-                const tokenInfo = response.data.data;
-                localStorage.setItem("token", tokenInfo.accessToken);
-                console.log(tokenInfo);
-                setLoggedIn(true);
-            })
+  const login = () => {
+    const json = {
+      email: email,
+      password: password,
     };
 
-    if (loggedIn) {
-        navigate("/month", {state: {email: email}});
-    }
+    console.log(json);
 
-    return (
-        <div>
-            <Container>
-                <Box
-                    sx={{
-                        display: "flex",
-                        flexDirection: "column",
-                        justifyContent: "center",
-                        alignItems: "center",
-                        alignContent: "center",
-                    }}
-                >
-                    <h1>login</h1>
-                    <Grid xs={8} sx={{m: 2}}>
-                        <TextField
-                            id="outlined-basic"
-                            label="username"
-                            variant="outlined"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                        />
-                    </Grid>
-                    <Grid xs={8} sx={{m: 2}}>
-                        <TextField
-                            id="outlined-password-input"
-                            label="Password"
-                            variant="outlined"
-                            value={password}
-                            type="password"
-                            autoComplete="current-password"
-                            onChange={(e) => setPassword(e.target.value)}
-                        />
-                    </Grid>
-                    <Grid xs={8} sx={{mt: 0.5, mb: 2}}>
-                        <Button onClick={login} variant="contained">
-                            Login
-                        </Button>
-                    </Grid>
-                </Box>
-            </Container>
-        </div>
-    );
+    axios.post("/api/login", json).then((response) => {
+      const tokenInfo = response.data.data;
+      localStorage.setItem("token", tokenInfo.accessToken);
+      console.log(tokenInfo);
+      setLoggedIn(true);
+    });
+  };
+
+  if (loggedIn) {
+    navigate("/month", { state: { email: email } });
+  }
+
+  return (
+    <div>
+      <Container>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            alignContent: "center",
+          }}
+        >
+          <h1>login</h1>
+          <Grid xs={8} sx={{ m: 2 }}>
+            <TextField
+              id="outlined-basic"
+              label="username"
+              variant="outlined"
+              value={email}
+              type="textfield"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </Grid>
+          <Grid xs={8} sx={{ m: 2 }}>
+            <TextField
+              id="outlined-password-input"
+              label="Password"
+              variant="outlined"
+              value={password}
+              type="password"
+              autoComplete="current-password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </Grid>
+          <Grid xs={8} sx={{ mt: 0.5, mb: 2 }}>
+            <Button onClick={login} variant="contained">
+              Login
+            </Button>
+          </Grid>
+        </Box>
+      </Container>
+    </div>
+  );
 };
 
 export default Login;
