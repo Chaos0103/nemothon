59ce94bb6dde54caa68414cd86aae9b605d55a47
leeyr0412 <leeyr0412@naver.com>
2023-09-01 04:16:57 +0900

    feat: 데일리

--- client/src/components/DailyPlan.jsx
+++ client/src/components/DailyPlan.jsx
@@ -6,6 +6,7 @@ import 'tui-calendar/dist/tui-calendar.css';
 import 'tui-date-picker/dist/tui-date-picker.css';
 import 'tui-time-picker/dist/tui-time-picker.css';
 import axios from "axios";
+import {CalendarInfo} from "@toast-ui/calendar/types/types/options";
 
 // import axios from "axios";
 
@@ -18,24 +19,28 @@ import axios from "axios";
 class DailyPlan extends React.Component {
     // const [categories, setCategories] = useState([]);
 
-    state = {
-        events: []
-    }
+    // state = {
+    //     events: []
+    // }
 
     calendarRef = React.createRef();
 
-    // constructor() {
-    //     super();
-    //     axios.get(`http://localhost:8080/api/event/day/2023-08-31`, {
-    //         headers: {
-    //             Authorization: localStorage.getItem("token")
-    //         }
-    //     })
-    //         .then((response) => {
-    //             console.log("이벤트 목록 = ", response.data)
-    //             setEvents(response.data);
-    //         });
-    // }
+    constructor() {
+        super();
+        // axios.get(`http://localhost:8080/api/event/day/2023-08-31`, {
+        //     headers: {
+        //         Authorization: localStorage.getItem("token")
+        //     }
+        // })
+        //     .then((response) => {
+        //         console.log("이벤트 목록 = ", response.data)
+        //         setEvents(response.data);
+        //     });
+        this.state = {
+            events: [],
+            CalendarInfo: []
+        }
+    }
 
 
     handleClickNextButton = () => {
@@ -50,51 +55,38 @@ class DailyPlan extends React.Component {
         calendarInstance.prev();
         let showDate = calendarInstance.getDate().toUTCString();
         console.log("표시날짜 = {}", showDate);
-        // axios
-        //         .get(`http://localhost:8080/api/event/day/2023-08-31`, {
-        //             headers: {
-        //                 Authorization: localStorage.getItem("token")
-        //             }
-        //         })
-        //         .then((response) => {
-        //             console.log("이벤트 목록 = {}", response.data)
-        //             setEvents(response.data);
-        //         });
-    };
-
-    // const btnPrev = () => {
-    //     const calendarInstance = this.calenderRef.current.getInstance();
-    // }
-
-    // useEffect(() => {
-    //     axios
-    //         .get(`http://localhost:8080/categories`, {
-    //             headers: {
-    //                 Authorization: sessionStorage.getItem("jwt")
-    //             }
-    //         })
-    //         .then((response) => {
-    //             console.log(response.data);
-    //             setCategories(response.data);
-    //         });
-    //
-    //     axios
-    //         .get(`http://localhost:8080/api/event/day/2023-08-31`, {
-    //             headers: {
-    //                 Authorization: localStorage.getItem("token")
-    //             }
-    //         })
-    //         .then((response) => {
-    //             console.log("이벤트 목록 = {}", response.data)
-    //             setEvents(response.data);
-    //         });
-    // }, []);
+    }
 
     async componentDidMount() {
         const calendarInstance = this.calendarRef.current.getInstance();
+        //TODO: 상단바 동기 변경
         const showDate = toStringByFormatting(calendarInstance.getDate());
-        console.log("표시날짜 = ", showDate);
+        console.log("componentDidMount 실행 달력 표시날짜 = ", showDate);
         let token = localStorage.getItem("token");
+
+        // 카테고리 가져오기
+        await axios
+            .get(`/api/categories`, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            }).then((response) => {
+                console.log("등록된 카테고리 불러옴", response.data.data)
+                this.state.calendars = response.data.data;
+                //카테고리 여러개 왔음
+                response.data.data.map(function (i) {
+                    //id:String, name:String 으로 CalendarInfo 만듦
+                    this.state.CalendarInfo.push()
+                    //CalendarInfo 배열을 아래코드로 라이브러리에 등록
+                    // calendarInstance.setCalendars(this.state.CalendarInfo)
+                })
+                //반복 끝나면 다시 배열 돌리면서 색상 정해줘야함
+                response.data.data.map(function (i) {
+                    calendarInstance.setCalendarColor(this.state.CalendarInfo.id, "#" + response.data.data.color);
+                })
+            });
+
+        //일정 부분 완료됨
         await axios
             .get(`/api/event/day/${showDate}`, {
                 headers: {
@@ -102,10 +94,19 @@ class DailyPlan extends React.Component {
                 }
             })
             .then((response) => {
-                console.log("이벤트 목록 =", response.data)
-                this.setState({events: response.data});
-            }).then(calendarInstance.createEvents(this.state.events));
-        console.log(this.state.events);
+                console.log("이벤트 목록 =", response.data.data)
+                this.state.events = response.data.data;
+                // this.setState({events: response.data.data});
+            }).then(() => {
+                calendarInstance.createEvents(this.state.events);
+                console.log("스테이트 저장함 이벤트 목록 =", this.state.events)
+            })
+            .then(() => {
+                calendarInstance.cale
+                // calendarInstance.setCalendarColor()
+
+            })
+        // console.log(this.state.events);
 
     }
 
@@ -123,7 +124,7 @@ class DailyPlan extends React.Component {
                             name: 'Private',
                             bgColor: '#9e5fff',
                             borderColor: '#9e5fff',
-                            color:'#9e5fff'
+                            color: '#9e5fff'
                         },
                         {
                             id: '1',
@@ -133,32 +134,32 @@ class DailyPlan extends React.Component {
                         }
                     ]}
                     isReadOnly={true}
-                    events={[
-                        {
-                            id: 'event1',
-                            calendarId: '0',
-                            title: '주간 회의',
-                            start: '2023-08-31T09:00:00',
-                            end: '2023-08-31T10:00:00',
-                            goingDuration: 70,
-                        },
-                        {
-                            id: 'event1',
-                            calendarId: '0',
-                            title: '네모톤 참여',
-                            start: '2023-09-01T09:00:00',
-                            end: '2023-09-01T14:00:00',
-                            goingDuration: 20,
-                        },
-                        {
-                            id: 'event1',
-                            calendarId: 'cal2',
-                            title: '인천에서 놀기',
-                            start: '2023-09-01T18:00:00',
-                            end: '2023-09-01T20:00:00',
-                            goingDuration: 120,
-                        }]
-                    }
+                    // events={[
+                    //     {
+                    //         id: 'event1',
+                    //         calendarId: '0',
+                    //         title: '주간 회의',
+                    //         start: '2023-08-31T09:00:00',
+                    //         end: '2023-08-31T10:00:00',
+                    //         goingDuration: 70,
+                    //     },
+                    //     {
+                    //         id: 'event1',
+                    //         calendarId: '0',
+                    //         title: '네모톤 참여',
+                    //         start: '2023-09-01T09:00:00',
+                    //         end: '2023-09-01T14:00:00',
+                    //         goingDuration: 20,
+                    //     },
+                    //     {
+                    //         id: 'event1',
+                    //         calendarId: 'cal2',
+                    //         title: '인천에서 놀기',
+                    //         start: '2023-09-01T18:00:00',
+                    //         end: '2023-09-01T20:00:00',
+                    //         goingDuration: 120,
+                    //     }]
+                    // }
                     useDetailPopup={true}
                     week={{
                         // dayNames: ["일", "월", "화", "수", "목", "금", "토"],
@@ -172,7 +173,9 @@ class DailyPlan extends React.Component {
     }
 }
 
-function leftPad(value) {
+function
+
+leftPad(value) {
     if (value >= 10) {
         return value;
     }
@@ -180,7 +183,9 @@ function leftPad(value) {
     return `0${value}`;
 }
 
-function toStringByFormatting(source, delimiter = '-') {
+function
+
+toStringByFormatting(source, delimiter = '-') {
     const year = source.getFullYear();
     const month = leftPad(source.getMonth() + 1);
     const day = leftPad(source.getDate());
