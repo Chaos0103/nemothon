1648edc5acc0670c15a4d35432f3f3f4de4248be
leeyr0412 <leeyr0412@naver.com>
2023-08-31 18:33:53 +0900

    refactor: 수정

--- client/src/components/DailyPlan.jsx
+++ client/src/components/DailyPlan.jsx
@@ -1,87 +1,166 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 
 import Calendar from '@toast-ui/react-calendar';
 import 'tui-calendar/dist/tui-calendar.css';
 
 import 'tui-date-picker/dist/tui-date-picker.css';
 import 'tui-time-picker/dist/tui-time-picker.css';
-//         // eventView:  'task',
-//     },
-//     calendars: [
-//         {
-//             id: 'cal1',
-//             name: '개인',
-//             backgroundColor: '#03bd9e',
-//         },
-//         {
-//             id: 'cal2',
-//             name: '직장',
-//             backgroundColor: '#00a9ff',
-//         },
-//     ],
-// };
-//
-// const container = document.getElementById('calendar');
-// const calendar = new Calendar(container, options);
-//
-// calendar.render();
-//
-// calendar.createEvents([
-//     {
-//         id: 'event1',
-//         calendarId: 'cal2',
-//         title: '주간 회의',
-//         start: '2023-08-29T09:00:00',
-//         end: '2023-08-29T10:00:00',
-//         goingDuration: 70,
-//     },
-//     {
-//         id: 'event2',
-//         calendarId: 'cal1',
-//         title: '점심 약속',
-//         start: '2023-08-29T12:00:00',
-//         end: '2023-08-29T13:00:00',
-//     },
-// ]);
+import axios from "axios";
 
-const DailyPlan = () => {
-    // docutc. ~~~~ 지양
-    // 요소선택 = useRef
+// import axios from "axios";
 
-    return (
-        <div>
-            {/*<div id="calendar" style={{height: "600px"}}></div>*/}
-            <Calendar
-                view={"day"}
-                height="700px"
-                calendars={[
-                    {
-                        id: '0',
-                        name: 'Private',
-                        bgColor: '#9e5fff',
-                        borderColor: '#9e5fff'
-                    },
-                    {
-                        id: '1',
-                        name: 'Company',
-                        bgColor: '#00a9ff',
-                        borderColor: '#00a9ff'
-                    }
-                ]}
-                isReadOnly={true}
-                events={[
-                    {
-                        id: 'event1',
-                        calendarId: 'cal2',
-                        title: '주간 회의',
-                        start: '2023-08-31T09:00:00',
-                        end: '2023-08-31T10:00:00',
-                        goingDuration: 70,
-                    }]
+
+// this.calendarRef =
+
+// this.calenderRef = React.createRef();
+// const [events, setEvents] = useState([]);
+
+class DailyPlan extends React.Component {
+    // const [categories, setCategories] = useState([]);
+
+    state = {
+        events: []
+    }
+
+    calendarRef = React.createRef();
+
+    // constructor() {
+    //     super();
+    //     axios.get(`http://localhost:8080/api/event/day/2023-08-31`, {
+    //         headers: {
+    //             Authorization: localStorage.getItem("token")
+    //         }
+    //     })
+    //         .then((response) => {
+    //             console.log("이벤트 목록 = {}", response.data)
+    //             setEvents(response.data);
+    //         });
+    // }
+
+
+    handleClickNextButton = () => {
+        const calendarInstance = this.calendarRef.current.getInstance();
+
+        calendarInstance.next();
+    };
+
+    handleClickPrevButton = () => {
+        const calendarInstance = this.calendarRef.current.getInstance();
+
+        calendarInstance.prev();
+        let showDate = calendarInstance.getDate().toUTCString();
+        console.log("표시날짜 = {}", showDate);
+        // axios
+        //         .get(`http://localhost:8080/api/event/day/2023-08-31`, {
+        //             headers: {
+        //                 Authorization: localStorage.getItem("token")
+        //             }
+        //         })
+        //         .then((response) => {
+        //             console.log("이벤트 목록 = {}", response.data)
+        //             setEvents(response.data);
+        //         });
+    };
+
+    // const btnPrev = () => {
+    //     const calendarInstance = this.calenderRef.current.getInstance();
+    // }
+
+    // useEffect(() => {
+    //     axios
+    //         .get(`http://localhost:8080/categories`, {
+    //             headers: {
+    //                 Authorization: sessionStorage.getItem("jwt")
+    //             }
+    //         })
+    //         .then((response) => {
+    //             console.log(response.data);
+    //             setCategories(response.data);
+    //         });
+    //
+    //     axios
+    //         .get(`http://localhost:8080/api/event/day/2023-08-31`, {
+    //             headers: {
+    //                 Authorization: localStorage.getItem("token")
+    //             }
+    //         })
+    //         .then((response) => {
+    //             console.log("이벤트 목록 = {}", response.data)
+    //             setEvents(response.data);
+    //         });
+    // }, []);
+
+    componentDidMount() {
+        const calendarInstance = this.calendarRef.current.getInstance();
+        const showDate = toStringByFormatting(calendarInstance.getDate());
+        console.log("표시날짜 = ", showDate);
+        axios
+            .get(`/api/event/day/${showDate}`, {
+                headers: {
+                    Authorization: localStorage.getItem("token")
                 }
-            />
-        </div>
-    )
+            })
+            .then((response) => {
+                console.log("이벤트 목록 = {}", response.data)
+                this.state.events = response.data;
+                // setEvents(response.data);
+            });
+    }
+
+    render() {
+        return (
+            <div>
+                <div className='top-bar'></div>
+                <Calendar
+                    ref={this.calendarRef}
+                    view={"day"}
+                    height="700px"
+                    calendars={[
+                        {
+                            id: '0',
+                            name: 'Private',
+                            bgColor: '#9e5fff',
+                            borderColor: '#9e5fff'
+                        },
+                        {
+                            id: '1',
+                            name: 'Company',
+                            bgColor: '#00a9ff',
+                            borderColor: '#00a9ff'
+                        }
+                    ]}
+                    isReadOnly={true}
+                    events={
+                        this.state.events
+
+                    }
+                    useDetailPopup={true}
+                    week={{
+                        // dayNames: ["일", "월", "화", "수", "목", "금", "토"],
+                        taskView: false
+                    }}
+                />
+                <button onClick={this.handleClickPrevButton}>prev</button>
+                <button onClick={this.handleClickNextButton}>next</button>
+            </div>
+        );
+    }
+}
+
+function leftPad(value) {
+    if (value >= 10) {
+        return value;
+    }
+
+    return `0${value}`;
+}
+
+function toStringByFormatting(source, delimiter = '-') {
+    const year = source.getFullYear();
+    const month = leftPad(source.getMonth() + 1);
+    const day = leftPad(source.getDate());
+
+    return [year, month, day].join(delimiter);
 }
 
 export default DailyPlan;
--- server/src/main/java/com/nemo/server/api/controller/event/response/MonthEventResponse.java
+++ server/src/main/java/com/nemo/server/api/controller/event/response/MonthEventResponse.java
@@ -11,7 +11,7 @@ import java.time.format.DateTimeFormatter;
 @NoArgsConstructor
 public class MonthEventResponse {
 
-    private Long eventId;
+    private Long id;
     private Long categoryId;
     private String title;
     private String start;
@@ -19,8 +19,8 @@ public class MonthEventResponse {
     private int goingDuration;
 
     @Builder
-    public MonthEventResponse(Long eventId, Long categoryId, String title, LocalDateTime start, LocalDateTime end, int goingDuration) {
-        this.eventId = eventId;
+    public MonthEventResponse(Long id, Long categoryId, String title, LocalDateTime start, LocalDateTime end, int goingDuration) {
+        this.id = id;
         this.categoryId = categoryId;
         this.title = title;
         this.start = start.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
